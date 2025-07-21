39648be16bca43101f2d7775d303212fa2b46e82
Alex <alexwang@pinterest.com>
2025-07-20 21:52:41 -0500

    working bpe train

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -17,7 +17,11 @@ def train_bpe_tokenizer(input_path, vocab_size, special_tokens):
     escaped_tokens = [re.escape(token) for token in special_tokens]
     pattern = "|".join(escaped_tokens)
 
-    vocab = {bytes([i]): i for i in range(256)}
+    vocab = {i: bytes([i]) for i in range(256)}
+    for special_token in special_tokens:
+        new_idx = len(vocab)
+        vocab[new_idx] = special_token.encode('utf-8')
+
     merges = []
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
@@ -32,8 +36,9 @@ def train_bpe_tokenizer(input_path, vocab_size, special_tokens):
 
             for match in re.finditer(PAT, chunk):
                 pretoken = match.group(0)
-                pretoken_bytes = list(pretoken.encode('utf-8'))
-                pretoken_freq[tuple(pretoken_bytes)] += 1
+                pretoken_bytes = pretoken.encode('utf-8')
+                pretoken_bytes_tuple = tuple(bytes([b]) for b in pretoken_bytes)
+                pretoken_freq[pretoken_bytes_tuple] += 1
 
         while len(vocab) < vocab_size:
 
@@ -45,11 +50,10 @@ def train_bpe_tokenizer(input_path, vocab_size, special_tokens):
 
                 for i in range(len(pretoken_bytes) - 1):
 
-                    # print(type(pretoken_bytes))
-
                     try:
                         b1 = pretoken_bytes[i]
                         b2 = pretoken_bytes[i + 1]
+                        # print(f"Checking bytes: {b1}, {b2} ---- types: {type(b1)}, {type(b2)}")
                     except Exception as e:
                         print(pretoken_bytes)
                         raise e
@@ -59,14 +63,13 @@ def train_bpe_tokenizer(input_path, vocab_size, special_tokens):
             
             max_pairs = [pair for pair, count in byte_pair_freq.items() if count == max_count]
             pair_to_merge = max(max_pairs)
-            # print(pair_to_merge)
             
             merges.append(pair_to_merge)
 
             # Add the merged pair to the vocabulary
             new_idx = len(vocab)
             merged_pair = pair_to_merge[0] + pair_to_merge[1]
-            vocab[merged_pair] = new_idx
+            vocab[new_idx] = merged_pair
 
             print(f"Adding merge: {pair_to_merge} with index {new_idx}")
 
@@ -106,7 +109,10 @@ def train_bpe_tokenizer(input_path, vocab_size, special_tokens):
     return vocab, merges
     
 
-train_bpe_tokenizer(input_path="../data/TinyStoriesV2-GPT4-valid.txt", vocab_size=300, special_tokens=["<|endoftext|>"])
+# vocab, merges = train_bpe_tokenizer(input_path="../data/TinyStoriesV2-GPT4-valid.txt", vocab_size=1000, special_tokens=["<|endoftext|>"])
+
+# print(merges)
+# print(vocab)
 
 class BPETokenizer:
     """
