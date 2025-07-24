9b0e9cdd91b5cdf00a9a7d2c1f5c24de2331e175
Alex <alexwang@pinterest.com>
2025-07-24 15:53:55 -0500

    comments

--- cs336_basics/rope.py
+++ cs336_basics/rope.py
@@ -24,15 +24,18 @@ class RotaryPositionalEmbeddings(torch.nn.Module):
         theta = 1.0 / (
             self.base
             ** (torch.arange(0, self.dim, 2)[: (self.dim // 2)].float() / self.dim)
-        )
+        ) # [dim // 2]
         self.register_buffer("theta", theta, persistent=False)
         self.build_rope_cache(self.max_seq_len)
 
     def build_rope_cache(self, max_seq_len: int = 4096) -> None:
         seq_idx = torch.arange(
             max_seq_len, dtype=self.theta.dtype, device=self.theta.device
-        )
-        idx_theta = torch.einsum("i, j -> ij", seq_idx, self.theta).float()
+        ) # [max_seq_len]
+
+        # Create a matrix where each row is the sequence index multiplied by the theta values.
+        idx_theta = torch.outer(seq_idx, self.theta)  # [max_seq_len, dim // 2]
+
         cache_cos = torch.cos(idx_theta)  # [max_seq_len, dim // 2]
         cache_sin = torch.sin(idx_theta)  # [max_seq_len, dim // 2]
         self.register_buffer("cache_cos", cache_cos, persistent=False)
@@ -53,16 +56,15 @@ class RotaryPositionalEmbeddings(torch.nn.Module):
         """
         # Get input shape
         *batch_shape, seq_len, d_k = x.shape
+
         assert d_k == self.dim, f"Expected input last dim {self.dim}, got {d_k}"
 
-        if token_positions.max() >= self.cache_cos.size(0):
-            self.build_rope_cache(int(token_positions.max().item()) + 1)
-
-        # Slice cos/sin caches with token positions: (..., seq_len, dim // 2)
-        cos = self.cache_cos[token_positions]  # (..., seq_len, dim // 2)
-        sin = self.cache_sin[token_positions]  # (..., seq_len, dim // 2)
+        # Slice cos/sin caches with token positions
+        cos = self.cache_cos[token_positions]  # (seq_len, dim // 2)
+        sin = self.cache_sin[token_positions]  # (seq_len, dim // 2)
 
         # Reshape x to (..., seq_len, d_k//2, 2)
+        # For each token embedding of size d_k, we group adjacent elements into pairs
         x_ = x.float().reshape(*batch_shape, seq_len, d_k // 2, 2)
         x_real = x_[..., 0]  # (..., seq_len, d_k // 2)
         x_imag = x_[..., 1]  # (..., seq_len, d_k // 2)
@@ -73,8 +75,8 @@ class RotaryPositionalEmbeddings(torch.nn.Module):
             sin = sin.unsqueeze(-3)
 
         # Apply RoPE rotation
-        out_real = x_real * cos - x_imag * sin
-        out_imag = x_imag * cos + x_real * sin
+        out_real = x_real * cos - x_imag * sin # (..., seq_len, d_k // 2)
+        out_imag = x_imag * cos + x_real * sin # (..., seq_len, d_k // 2)
 
         out = torch.stack((out_real, out_imag), dim=-1)  # (..., seq_len, d_k//2, 2)
         out = out.reshape(*batch_shape, seq_len, d_k)    # (..., seq_len, d_k)
